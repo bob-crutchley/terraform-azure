8041e3a18f96e34b4d708b91ee01319ea48691bc
bob-crutchley <bob.crutchley96@gmail.com>
2019-11-27 08:50:31 +0000

    moved outputs out of main

--- main.tf
+++ main.tf
@@ -37,11 +37,3 @@ resource "azurerm_virtual_network" "default" {
   resource_group_name = azurerm_resource_group.default.name
 }
 
-output "nginx_fqdn" {
-  value = module.nginx.nginx_fqdn
-}
-
-output "jenkins_fqdn" {
-  value = module.jenkins.fqdn
-}
-
---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "nginx_fqdn" {
+  value = module.nginx.nginx_fqdn
+}
+
+output "jenkins_fqdn" {
+  value = module.jenkins.fqdn
+}
+
