6dbbf643f5e0f209ce642c356e061f7f2b869ce1
bob-crutchley <bob.crutchley96@gmail.com>
2019-11-28 11:14:40 +0000

    added nginx & jenkins

--- run-playbooks.sh
+++ run-playbooks.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+
+ansible-playbook \
+    -i "$(terraform output jenkins_fqdn)," \
+    -u jenkins \
+    --ssh-common-args="-o StrictHostKeyChecking=no" \
+    playbooks/jenkins.yml
+
+
 # NGINX
 ansible-playbook \
     -i "$(terraform output nginx_fqdn)," \
     -u jenkins \
     --ssh-common-args="-o StrictHostKeyChecking=no" playbooks/nginx.yml
-
